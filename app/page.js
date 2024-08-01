48eed103fdc3ae2be9eec7b8425effda2fa1e148
mzsami <163679075+mzsami@users.noreply.github.com>
2024-07-31 22:05:50 -0400

    Setup the firebase and next js files. Also added a little bit of content to page.js

---
+++ app/page.js
@@ -0,0 +1,16 @@
+'use client'
+import Image from "next/image";
+import {useState, useEffect} from 'react'
+import {firestore} from '@/firebase'
+import { Box, Typography } from "@mui/material";
+
+
+export default function Home() {
+  const {inventory, setInventory} = useState({})
+  const {open, setOpen} = useState(false)
+  const {itemName, setItemName} = useState('')
+  return <Box>
+    <Typography variant='h1'>Inventory Management</Typography>
+     </Box>
+}
+
---
+++ firebase.js
@@ -0,0 +1,25 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import {getFirestore} from 'firebase/firestore';
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyAlRiQH9Ol5Ctmuj_XlD8ZZI0HD06C_eDc",
+  authDomain: "inventory-management-f978a.firebaseapp.com",
+  projectId: "inventory-management-f978a",
+  storageBucket: "inventory-management-f978a.appspot.com",
+  messagingSenderId: "1089586767970",
+  appId: "1:1089586767970:web:80248305dac0a0715f093b",
+  measurementId: "G-BVECGBEDHN"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+const firestore = getFirestore(app)
+
+export {firestore}
